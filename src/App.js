36ff588ee24b674ae16b6faaf0f10f51d9bfd6d7
Devrath Rana <chintusinghrana1234567890@gmail.com>
2022-12-27 00:16:53 +0530

    file upload done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import FileUploader from "./FileUploader";
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("file", selectedFile);
+
+    console.log(selectedFile, name, formData);
+
+    // axios
+    //   .post(UPLOAD_URL, formData)
+    //   .then((res) => {
+    //     alert("File Upload success");
+    //   })
+    //   .catch((err) => alert("File Upload Error"));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitForm}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <FileUploader
+          onFileSelectSuccess={(file) => setSelectedFile(file)}
+          onFileSelectError={({ error }) => alert(error)}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
